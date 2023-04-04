20360079c0b7d6ae02dce73c25cf37fa9f52e0c0
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-04-04 22:08:18 +0000

    code challenge 12 complete

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,10 +1,43 @@
+from data_structures.stack.stack import Stack
+
+
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.cats = []
+        self.dogs = []
+        self.head = None
+        self.rear = None
+        # self.length = 0
+
+    def enqueue(self, animal):
+        species = animal.species
+        if species == 'cat':
+            self.cats.append(animal)
+
+        if species == 'dog':
+            self.dogs.append(animal)
+
+    def dequeue(self, pref):
+        if pref == 'dog':
+            popped = self.dogs.pop()
+            return popped
+
+        if pref == 'cat':
+            popped = self.cats.pop()
+            return popped
 
 
 class Dog:
-    pass
-
+    def __init__(self):
+        # self.name = ''
+        # self._next = _next
+        self.species = 'dog'
 
 class Cat:
-    pass
+    def __init__(self):
+        # self.name = ''
+        # self._next = _next
+        self.species = 'cat'
+
+if __name__ == '__main__':
+    new_dog = Dog()
